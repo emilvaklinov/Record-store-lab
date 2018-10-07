28319f908f0d77bdc636fe77eb0971d1c680c684
trakieca <emil_vaklinov@yahoo.com>
2018-10-07 17:58:47 +0100

    modules added

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+
+class Album(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    title = models.CharField(max_length=30)
+    year = models.CharField(max_length=30)
+    stock_level = models.CharField(max_length=30)
+    artist_id = models.CharField(max_length=20)
--- record_store/settings.py
+++ record_store/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'inventory',
+    # ...
+]
+
+INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
